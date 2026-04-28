9445bec784ba88bbec8b566dbedeffd3f1445501
aiaod1009 <3449508132@qq.com>
2026-04-28 11:32:01 +0800

    可用和不可用

--- 1.js
+++ 1.js
@@ -1,7 +1,13 @@
 // console.log('hello Node.js')
 // BOM
-console.log(window);
-console.log(history);
-console.log(location);
-// DOM
-console.log(document);
+// console.log(window);
+// console.log(history);
+// console.log(location);
+// // DOM
+// console.log(document);
+
+
+console.log('i love you')
+setTimeout(() => {
+  console.log('love too')
+}, 1000)
