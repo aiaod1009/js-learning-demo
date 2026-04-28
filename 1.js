19fb09b432578ed9f783aea9433168c0d47b637b
aiaod1009 <3449508132@qq.com>
2026-04-28 21:17:12 +0800

    6

--- 1.js
+++ 1.js
@@ -25,5 +25,8 @@
 // let buf3 = Buffer.from('hello')
 // console.log(buf3);
 
-let buf4 = Buffer.from([105, 108, 111, 118, 101, 121, 111, 117]);
-console.log(buf4.toString());
+// let buf4 = Buffer.from([105, 108, 111, 118, 101, 121, 111, 117]);
+// console.log(buf4.toString());
+
+// let buf5 = Buffer.from('hello');
+// console.log(buf5[0].toString(16));  // 几进制
