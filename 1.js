3e30b62bd0e00eb7aff38145e8ac1ee2888590f5
aiaod1009 <3449508132@qq.com>
2026-04-28 12:17:23 +0800

    4

--- 1.js
+++ 1.js
@@ -7,11 +7,20 @@
 // console.log(document);
 
 
-console.log('i love you')
-setTimeout(() => {
-  console.log('love too')
-}, 1000)
+// console.log('i love you')
+// setTimeout(() => {
+//   console.log('love too')
+// }, 1000)
 
-// global顶级对象
-console.log(global);
-console.log(globalThis === global);
+// // global顶级对象
+// console.log(global);
+// console.log(globalThis === global);
+
+let buf = Buffer.alloc(10);
+console.log(buf);
+
+let buf2 = Buffer.allocUnsafe(10);
+console.log(buf2);
+
+let buf3 = Buffer.from('hello')
+console.log(buf3);
