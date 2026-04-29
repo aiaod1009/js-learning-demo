7a3bf8728104696c53444e8ac62046ea326665e6
aiaod1009 <3449508132@qq.com>
2026-04-29 11:41:23 +0800

    同步读取

--- 2.js
+++ 2.js
@@ -8,10 +8,15 @@ ws.write('问渠哪得清如许？\n');
 ws.write('为有源头活水来。\n');
 ws.close();
 
-fs.readFile('./观书有感.txt', (err, data) => {
-  if (err) {
-    console.log('读取失败');
-    return;
-  }
-  console.log(data.toString());
-});
+// 异步读取
+// fs.readFile('./观书有感.txt', (err, data) => {
+//   if (err) {
+//     console.log('读取失败');
+//     return;
+//   }
+//   console.log(data.toString());
+// });
+
+// 同步读取
+let data = fs.readFileSync('./观书有感.txt');
+console.log(data.toString());
