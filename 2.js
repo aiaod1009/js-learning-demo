0b210b2376590b4201cac76ac5db8d6ee820a700
aiaod1009 <3449508132@qq.com>
2026-04-29 12:30:42 +0800

    文件的操作

--- 2.js
+++ 2.js
@@ -79,7 +79,53 @@ const fs = require('fs');
 
 
 // 调用 rm 方法
-fs.rm('./资料/hi.txt', err => {
+// fs.rm('./资料/hi.txt', err => {
+//   if (err) {
+//     console.log('删除失败');
+//     return;
+//   }
+//   console.log('删除成功');
+// });
+
+// 创建文件夹
+// fs.mkdir('./html', err => {
+//   if (err) {
+//     console.log('创建失败');
+//     return;
+//   }
+//   console.log('创建成功');
+// });
+
+// 递归创建
+// fs.mkdir('./a/b/c', { recursive: true }, err => {
+//   if (err) {
+//     console.log('创建失败');
+//     return;
+//   }
+//   console.log('创建成功');
+// });
+
+
+// 读取文件夹
+// fs.readdir('./', (err, data) => {
+//   if (err) {
+//     console.log('读取失败');
+//     return;
+//   }
+//   console.log(data);
+// });
+
+// 删除文件夹
+// fs.rmdir('./html', err => {
+//   if (err) {
+//     console.log('删除失败');
+//     return;
+//   }
+//   console.log('删除成功');
+// });
+
+// 递归删除
+fs.rm('./a', { recursive: true }, err => {
   if (err) {
     console.log('删除失败');
     return;
