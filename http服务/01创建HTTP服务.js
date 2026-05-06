3ee6e2a4620d03c598408af378197475225b3637
aiaod1009 <3449508132@qq.com>
2026-05-06 23:06:13 +0800

    5

--- http服务/01创建HTTP服务.js
+++ http服务/01创建HTTP服务.js
@@ -3,8 +3,11 @@ const http = require('http');
 
 // 2. 创建服务对象
 const server = http.createServer((request, response) => {
-  response.end('Hello HTTP!'); // 设置响应体
+  // response.end('Hello HTTP!'); // 设置响应体
+  response.setHeader('Content-Type', 'text/html; charset=utf-8'); // 设置响应头，指定编码格式
+  response.end('你好')
 });
+// 如果有中文会乱码
 
 // 3. 监听端口，启动服务
 server.listen(3000, () => {
