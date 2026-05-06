1416915217afcf4b0078ac95cec0c0ec89424422
aiaod1009 <3449508132@qq.com>
2026-05-06 22:59:51 +0800

    4

--- http服务/01创建HTTP服务.js
+++ http服务/01创建HTTP服务.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 // 2. 创建服务对象
 const server = http.createServer((request, response) => {
-  response.end('Hello HTTP Server!'); // 设置响应体
+  response.end('Hello HTTP!'); // 设置响应体
 });
 
 // 3. 监听端口，启动服务
