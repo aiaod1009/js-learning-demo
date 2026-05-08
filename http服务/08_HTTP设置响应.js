14bb69ae36c526b9b26fc0a2ccd4b435286337a8
aiaod1009 <3449508132@qq.com>
2026-05-08 16:49:23 +0800

    2

--- http服务/08_HTTP设置响应.js
+++ http服务/08_HTTP设置响应.js
@@ -3,7 +3,9 @@ const http = require('http');
 
 // 2. 创建服务对象
 const server = http.createServer((request, response) => {
-  response.end('response') // 设置响应体
+  response.statusCode = 203; // 设置响应状态码
+  response.statusMessage = 'i love you'; // 响应状态的描述
+  response.end('response'); // 设置响应体
 });
 
 // 3. 监听端口，启动服务
