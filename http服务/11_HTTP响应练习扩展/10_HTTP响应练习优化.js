1a64e6a6698c480e25466d2c282678f3f7bef987
aiaod1009 <3449508132@qq.com>
2026-05-08 19:02:54 +0800

    7

--- http服务/11_HTTP响应练习扩展/10_HTTP响应练习优化.js
+++ http服务/11_HTTP响应练习扩展/10_HTTP响应练习优化.js
@@ -4,9 +4,19 @@ const fs = require('fs');
 
 // 创建服务对象
 const server = http.createServer((request, response) => {
-  let html = fs.readFileSync(__dirname + '/test.html');
-
-  response.end(html); // 设置响应体
+  let { pathname } = new URL(request.url, 'http://127.0.0.1');
+  if (pathname === '/') {
+    let html = fs.readFileSync(__dirname + '/test.html');
+    response.end(html); // 设置响应体
+  } else if (pathname === '/index.js') {
+    let js = fs.readFileSync(__dirname + '/index.js');
+    response.end(js); // 设置响应体
+  } else if (pathname === '/index.css') {
+    let css = fs.readFileSync(__dirname + '/index.css');
+    response.end(css); // 设置响应体
+  } else {
+    response.end('Not Found');
+  }
 });
 
 // 3. 监听端口，启动服务
