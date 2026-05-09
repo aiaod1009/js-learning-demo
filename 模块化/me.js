2e68cbf8b866d7728619d6a596a9919d268b1680
aiaod1009 <3449508132@qq.com>
2026-05-09 21:11:48 +0800

    9

--- 模块化/me.js
+++ 模块化/me.js
@@ -16,4 +16,9 @@ function niejiao() {
 //exports 暴露数据
 // exports.tiemo = tiemo;
 // exports.niejiao = niejiao;
-module.exports = '123'
+
+//可以暴露任意数据
+// module.exports = '123'
+
+// 不能使用exports = value的形式暴露数据
+exports = '11'
