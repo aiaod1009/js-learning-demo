cb30d978eac8d1e3b2b1d64c8b2f82666432855c
aiaod1009 <3449508132@qq.com>
2026-05-10 11:21:51 +0800

    5

---
+++ 包管理工具/01_npm/03.js
@@ -0,0 +1,22 @@
+// 1. 导入http模块
+const http = require('http');
+
+// 2. 创建服务对象
+const server = http.createServer((request, response) => {
+  // 获取请求的方法
+  // console.log(request.method);
+  // 获取请求的URL
+  console.log(request.url);
+  // 获取HTTP协议的版本号
+  console.log(request.httpVersion);
+  // 获取请求头
+  console.log(request.headers);
+  // 可以.host
+  response.end('Hello HTTP!nodemon'); // 设置响应体
+});
+// 如果有中文会乱码
+
+// 3. 监听端口，启动服务
+server.listen(3000, () => {
+  console.log('服务已经启动....');
+});
