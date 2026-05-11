f2b008af61ef3d8227e46c4ba775ff4c76cff6c0
aiaod1009 <3449508132@qq.com>
2026-05-11 12:06:44 +0800

    6

---
+++ express/模板.js
@@ -0,0 +1,18 @@
+//引入express模块
+const express = require('express');
+
+//创建应用对象
+const app = express();
+
+//创建路由
+
+app.get("/:id.html", (req, res) => {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.end('商品详情');
+});
+
+
+//监听端口，启动服务
+app.listen(3000, () => {
+  console.log('服务已经启动,端口3000正在监听中....');
+});
