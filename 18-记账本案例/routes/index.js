3b11ce819f5503b7104a8d060375b1938358ea5f
aiaod1009 <3449508132@qq.com>
2026-05-12 22:04:28 +0800

    2

--- 18-记账本案例/routes/index.js
+++ 18-记账本案例/routes/index.js
@@ -11,7 +11,7 @@ router.get('/account/create', function (req, res, next) {
 });
 //新增记录
 router.post('/account', (req, res) => {
-  //获取请求题的数据
+  //获取请求体的数据
   console.log(req.body);
   res.send('添加记录')
 });
