a4253e979537b19b69578f53a3e76c9369bf9112
aiaod1009 <3449508132@qq.com>
2026-05-12 22:37:44 +0800

    7

--- 18-记账本案例/routes/index.js
+++ 18-记账本案例/routes/index.js
@@ -13,7 +13,9 @@ const shortid = require('shortid');
 
 //记账本列表
 router.get('/account', function (req, res, next) {
-  res.render('list');
+  //获取所有的账单信息
+  let accounts = db.get('accounts').value();
+  res.render('list', { accounts: accounts });
 });
 //添加记录
 router.get('/account/create', function (req, res, next) {
