1d6be3d8663a8844ec501e5211a4dd12ef42b57c
aiaod1009 <3449508132@qq.com>
2026-05-13 20:23:35 +0800

    5

--- mongo/04_字段验证.js
+++ mongo/04_字段验证.js
@@ -6,12 +6,22 @@ mongoose.connection.once('open', () => {
   // console.log('数据库连接成功了哦~~~');
   //设置集合中文档的属性以及属性值的类型
   let BookSchema = new mongoose.Schema({
-    name: String,
-    author: String,
+    name: {
+      type: String,
+      required: true  // 该属性必须不为空
+    },
+    author: {
+      type: String,
+      default: '佚名'  // 默认值
+    },
+    style: {
+      type: String,
+      enum: ['科幻', '小说', '文学']  // 枚举值
+    },
     price: Number,
     is_hot: Boolean,
-    tags: Array,
-    pub_time: Date
+    pub_time: Date,
+    unique: true  //唯一索引
   });
   //创建模型对象 对文档操作的封装对象
   let BookModel = mongoose.model('books', BookSchema);
@@ -21,11 +31,11 @@ mongoose.connection.once('open', () => {
     author: '刘慈欣',
     price: 99,
     is_hot: true,
-    tags: ['科幻', '小说'],
+    style: '科幻',
     pub_time: new Date()
   }, (err, data) => {
     if (err) {
-      console.log(err);
+      console.log('插入失败了哦~~~');
       return;
     }
     console.log(data);
