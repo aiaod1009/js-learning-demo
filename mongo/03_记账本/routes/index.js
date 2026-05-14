8e214aa74a3d0bbfaff83673c197238505880513
aiaod1009 <3449508132@qq.com>
2026-05-14 10:26:20 +0800

    1

---
+++ mongo/03_记账本/routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+//导入lowdb
+const low = require('lowdb');
+//导入lowdb的文件存储适配器
+const FileSync = require('lowdb/adapters/FileSync');
+//创建适配器对象，指定数据库文件存储在data目录下，文件名为db.json
+const adapter = new FileSync(__dirname + '/../data/db.json');
+// 获取db对象
+const db = low(adapter)
+//导入 shortid
+const shortid = require('shortid');
+
+//记账本列表
+router.get('/account', function (req, res, next) {
+  //获取所有的账单信息
+  let accounts = db.get('accounts').value();
+  res.render('list', { accounts: accounts });
+});
+//添加记录
+router.get('/account/create', function (req, res, next) {
+  res.render('create');
+});
+//新增记录
+router.post('/account', (req, res) => {
+  let id = shortid.generate();
+  db.get('accounts').unshift({ id: id, ...req.body }).write();
+  res.render('success', { msg: '添加成功哦~~~', url: '/account' });
+});
+
+//删除记录
+router.get('/account/:id', (req, res) => {
+  //获取params.id
+  let id = req.params.id;
+  db.get('accounts').remove({ id: id }).write();
+  res.render('success', { msg: '删除成功哦~~~', url: '/account' });
+})
+module.exports = router;
