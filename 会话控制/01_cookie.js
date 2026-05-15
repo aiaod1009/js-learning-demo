3fa0523060d0926ff9faaec98e46a4718dfe4a39
aiaod1009 <3449508132@qq.com>
2026-05-15 13:12:43 +0800

    2

--- 会话控制/01_cookie.js
+++ 会话控制/01_cookie.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
-
+app.use(cookieParser());
 // app.get('/set-cookie', (req, res) => {
 //   res.cookie('name', '张三')
 //   res.send('home');
 // });
 
+//创建路由规则
 app.get('/set-cookie', (req, res) => {
   res.cookie('name', '张三', { maxAge: 1000 * 60 * 60 }) //过期时间，单位毫秒
   res.cookie('tieme', 'blue')
@@ -18,4 +20,9 @@ app.get('/clear-cookie', (req, res) => {
   res.send('删除成功');
 });
 
+//获取 cookie
+app.get('/get-cookie', (req, res) => {
+  console.log(req.cookies);
+  res.send('获取cookie成功');
+});
 app.listen(3000);
